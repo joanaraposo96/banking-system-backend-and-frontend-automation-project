75f6a804fff0b55f94b87746a80d5a46a15ebdda
joanaraposo96 <joanaraposo96@outlook.pt>
2026-04-30 16:14:18 +0200

    Refactor account creation tests to improve clarity and structure

--- backend/account-creation.spec.js
+++ backend/account-creation.spec.js
@@ -1,14 +1,58 @@
+import { test, expect } from "@playwright/test";
+import { faker } from "@faker-js/faker";
+
 test.describe('Account Creation', () => {
 
     const user = {
         name: faker.person.fullName(),
         email: faker.internet.email(),
         cpf: `${faker.string.numeric(3)}.${faker.string.numeric(3)}.${faker.string.numeric(3)}-${faker.string.numeric(2)}`,
-        initialBalance: faker.number.float({ max: 100000, fractionDigits: 0}).toLocaleString('pt-PT') // Applies thousands separator (e.g., 100.000)
+        initialBalance: faker.number.int({ min: 0, max: 100000 })
     }
 
-    test('Successfully register a new account', async ({ request }) => {
-        const response = await request.post('/accounts', { data: user })
+    test('Should create a new account successfully', async ({ request }) => {
+        const response = await request.post('/accounts', { data: user });
+        const body = await response.json();
+        expect(response.status()).toBe(201);
+        expect(body).toHaveProperty('id');
+        expect(body).toHaveProperty('balance');
+        expect(body).toHaveProperty('createdAt');
+    });
+
+    test('Should return status 400 when initial balance is negative', async ({ request }) => {
+        const updatedUser = {
+            ...user,
+            initialBalance: faker.number.int({ min: -100000, max: -1 })
+        }
+
+        const response = await request.post('/accounts', { data: updatedUser });
+        const body = response.json();
+        expect(response.status()).toBe(400);
+    });
+
+    test('Should return status 400 when missing mandatory fields', async ({ request }) => {
+        const updatedUser = {
+            ...user,
+            name: ''
+        }
+
+        const response = await request.post('/accounts', { data: updatedUser });
+        const body = response.json();
+        expect(response.status()).toBe(400);
+    })
+
+    test('Should return status 400 when cpf is already registered', async ({ request }) => {
+        const responseNEW = await request.post('/accounts', { data: user });
+        const bodyNEW = await responseNEW.json();
+
+        const updatedUser = {
+            ...user,
+            cpf: bodyNEW.cpf
+        }
+
+        const responseDUPLICATE = await request.post('/accounts', { data: updatedUser });
+        const bodyDUPLICATE = responseDUPLICATE.json();
+        expect(responseDUPLICATE.status()).toBe(400);
     });
 
 });
